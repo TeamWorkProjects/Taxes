c2af5d7fa201c30ee1717ca771572babb9e50b7c
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2017-12-06 22:08:56 +0100

    correct directory

--- .scripts/dotnet-publish.sh
+++ .scripts/dotnet-publish.sh
@@ -1,2 +1,2 @@
-dotnet publish taxes.api/ -c Release -o /bin/Docker
-dotnet publish taxes.services/ -c Release -o /bin/Docker
+dotnet publish taxes.api/ -c Release -o bin/Docker
+dotnet publish taxes.services/ -c Release -o bin/Docker
