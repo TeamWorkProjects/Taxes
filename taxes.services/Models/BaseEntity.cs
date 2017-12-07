cbd9703a9598fb2bcdb162dd53a8a3274b002ca5
Maciej <wybranski.maciej@outlook.com>
2017-12-07 21:58:02 +0100

    Created two abstract class: BaseEntity and Company

---
+++ taxes.services/Models/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace taxes.services.Models
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public int Creater { get; set; } //User Id
+        public int Modifier { get; set; } //User Id
+    }
+}
---
+++ taxes.services/Models/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace taxes.services.Models
+{
+    public abstract class Company : BaseEntity
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string NIP { get; set; }
+    }
+}
